9f90c6efe484bf8194b8a28a0758cb36fe74fa5f
https://github.com/kushpedia/git-test.git <haronkuria2015@gmail.com>
2023-11-22 23:44:59 +0300

    search functionality

--- store/views.py
+++ store/views.py
@@ -4,6 +4,7 @@ from categoryApp.models import Category
 from carts.views import _cart_id
 from carts.models import Cart, CartItem
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+from django.db.models import Q
 
 # Create your views here.
 def store(request, category_slug = None):
@@ -43,3 +44,16 @@ def productDetails(request,category_slug, product_slug):
         'in_cart' : in_cart
     }
     return render(request, 'store/product_detail.html',context)
+
+
+def search(request):
+    if 'keyword' in request.GET:
+        keyword = request.GET['keyword']
+        if keyword:
+            products = Product.objects.order_by('-created_date').filter(Q(description__icontains=keyword) | Q(product_name__icontains=keyword))
+            product_count = products.count()
+    context = {
+        'products' : products,
+        'product_count' : product_count,
+    }
+    return render (request, 'store/store.html', context)
