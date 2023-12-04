f48b87909fdc2b89340df0d8fe7ce9dac438d062
kushpedia <haronkuria2015@gmail.com>
2023-12-04 17:14:14 +0300

    login and forgot passwprd resets

--- Kushpedia_E_Shop/settings.py
+++ Kushpedia_E_Shop/settings.py
@@ -138,3 +138,11 @@ from django.contrib.messages import constants as messages
 MESSAGE_TAGS = {
     messages.ERROR: "danger",
 }
+
+
+# SMTP CONFIGURATIONS
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'kushpedia2023py@gmail.com'
+EMAIL_HOST_PASSWORD = 'vaobxgmynxhtiixw'
--- accounts/views.py
+++ accounts/views.py
@@ -5,6 +5,16 @@ from . models import Account
 from django.contrib import messages, auth
 from django.contrib.auth.decorators import login_required
 
+# user activation
+from django.contrib.sites.shortcuts import get_current_site
+from django.template.loader import render_to_string
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.utils.encoding import force_bytes
+from django.contrib.auth.tokens import default_token_generator
+from django.core.mail import EmailMessage
+
+
+
 
 # Create your views here.
 def register(request):
@@ -17,12 +27,23 @@ def register(request):
             password = form.cleaned_data['password']
             email = form.cleaned_data['email']
             username = email.split("@")[0]
-
             user = Account.objects.create_user(first_name = first_name, last_name = last_name, email = email, username = username, password = password)
             user.phone_number = phone_number
             user.save()
-            messages.success(request, 'Registration Successful')
-            return redirect('register')
+
+            current_site = get_current_site(request)
+            mail_subject = 'Account Activation'
+            message = render_to_string('accounts/account_verification_email.html',{
+                'user' : user,
+                'domain' : current_site,
+                'uid' : urlsafe_base64_encode(force_bytes(user.pk)),
+                'token' : default_token_generator.make_token(user),
+            })
+            to_email = email
+            send_email = EmailMessage(mail_subject, message, to=[to_email])
+            send_email.send()
+            # messages.success(request, 'Registration Successful, check your email for activation')
+            return redirect('/accounts/login/?command=verification&email='+email)
     else:
         form = RegistrationForm()
     context = {
@@ -38,8 +59,8 @@ def login(request):
         user = auth.authenticate(email = email, password = password)
         if user is not None:
             auth.login(request,user)
-            # messages.success(request, 'You are logged in successfully')
-            return redirect('home')
+            messages.success(request, 'You are logged in successfully')
+            return redirect('dashboard')
         else:
             messages.error(request, 'Invalid Login Credentials')
 
@@ -50,3 +71,80 @@ def logout(request):
     auth.logout(request)
     messages.error(request, 'Logged Out Successful')
     return redirect('login')
+
+def activate(request,uidb64, token):
+    try:
+        uid = urlsafe_base64_decode(uidb64).decode()
+        user = Account._default_manager.get(pk=uid)
+    except(TypeError, ValueError,OverflowError, Account.DoesNotExist):
+        user = None
+
+    if user is not None and default_token_generator.check_token(user,token):
+        user.is_active = True
+        user.save()
+        messages.success(request, 'Your account is activated')
+        return redirect('login')
+    else:
+        messages.error(request, 'Invalid activation link')
+        return render('registration')
+    
+@login_required(login_url = 'login')    
+def dashboard(request):
+    return render(request, 'accounts/dashboard.html')
+
+
+def forgotPassword(request):
+    if request.method == 'POST':
+        email=request.POST['email']
+        if Account.objects.filter(email=email).exists():
+            user = Account.objects.get(email_exact =email)
+            current_site = get_current_site(request)
+            mail_subject = 'Password Reset'
+            message = render_to_string('accounts/reset_password_email.html',{
+                'user' : user,
+                'domain' : current_site,
+                'uid' : urlsafe_base64_encode(force_bytes(user.pk)),
+                'token' : default_token_generator.make_token(user),
+            })
+            to_email = email
+            send_email = EmailMessage(mail_subject, message, to=[to_email])
+            send_email.send()
+            messages.success('Password reset link has been sent to your email')
+            return redirect('login')
+        else:
+            messages.error("Account Does not exist")
+            return redirect('forgotPassword')
+
+    return render(request, 'accounts/forgotPassword.html')
+
+def resetPassword_validate(request, uidb64, token):
+    try:
+        uid = urlsafe_base64_decode(uidb64).decode()
+        user = Account._default_manager.get(pk=uid)
+    except(TypeError, ValueError,OverflowError, Account.DoesNotExist):
+        user = None
+    if user is not None and default_token_generator.check_token(user,token):
+        request.session['uid'] = uid
+        messages.success(request, 'Reset your password')
+        return redirect('resetPassword')
+    else:
+        messages.error(request, 'The link has expired')
+        return render('registration')
+    
+
+def resetPassword(request):
+    if request.method == 'POST':
+        password = request.POST['password']
+        confirm_password = request.POST['confirm_password']
+        if password == confirm_password:
+            uid = request.session.get('uid')
+            user = Account.objects.get(pk=uid)
+            user.set_password(password)
+            user.save()
+            messages.success(request, 'Passwords reset successful')
+            return redirect('login')
+        else:
+            messages.error(request, 'Passwords do not match')
+            return redirect('resetPassword')
+    else:
+        return render(request, 'accounts/resetPassword.html')
